60d2b1b6b22701477ad1d362c2a5cf56b8d6f565
BlazingTwist <39350649+BlazingTwist@users.noreply.github.com>
2023-03-12 15:12:34 +0100

    make BepInEx plugin for automatic version checking

---
+++ BTHarmonyUtils/src/BTHarmonyUtilsPlugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using JetBrains.Annotations;
+
+namespace BTHarmonyUtils {
+
+	/// <summary>
+	/// BTHarmonyUtils plugin
+	/// </summary>
+	[PublicAPI]
+	[BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+	public class BTHarmonyUtilsPlugin : BaseUnityPlugin {
+
+		/// <summary>
+		/// BTHU plugin guid
+		/// </summary>
+		public const string pluginGuid = "blazingtwist.harmonyutils";
+		
+		/// <summary>
+		/// BTHU plugin display-name
+		/// </summary>
+		public const string pluginName = "BT Harmony Utils";
+		
+		/// <summary>
+		/// BTHU plugin version
+		/// </summary>
+		public const string pluginVersion = "0.2.0";
+
+	}
+
+}
